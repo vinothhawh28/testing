9e60227e3b7562882c10eabff28fa8cc058db6da
vinothhawh28 <38490375+vinothhawh28@users.noreply.github.com>
2018-07-12 09:29:06 +0530

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class nfactorial {
+public static void main(String[] args) {
+	int a;
+	int c=0,temp=0;
+	Scanner in=new Scanner(System.in);
+	a=in.nextInt();
+	for(int i=1;i<=a;i++){
+		c=i*a;	
+	System.out.print(c+" ");}
+}
+}
